2395eed93b36c02320131f8d67f3b79d8f59b697
Chingis Radnaev <chingis.radnaew@mail.ru>
2018-06-11 13:53:45 +0700

    initial

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -28,13 +28,187 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.mainTextBox = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.открытьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.создатьНовыйToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.сохранитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.печатьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.colorBtn = new System.Windows.Forms.Button();
+            this.boldBtn = new System.Windows.Forms.Button();
+            this.italicBtn = new System.Windows.Forms.Button();
+            this.underlineBtn = new System.Windows.Forms.Button();
+            this.FontSizeUpDown = new System.Windows.Forms.NumericUpDown();
+            this.menuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.FontSizeUpDown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // mainTextBox
+            // 
+            this.mainTextBox.Enabled = false;
+            this.mainTextBox.Location = new System.Drawing.Point(12, 81);
+            this.mainTextBox.Name = "mainTextBox";
+            this.mainTextBox.Size = new System.Drawing.Size(776, 357);
+            this.mainTextBox.TabIndex = 0;
+            this.mainTextBox.Text = "";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.открытьToolStripMenuItem,
+            this.создатьНовыйToolStripMenuItem,
+            this.сохранитьToolStripMenuItem,
+            this.печатьToolStripMenuItem,
+            this.выходToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // открытьToolStripMenuItem
+            // 
+            this.открытьToolStripMenuItem.Name = "открытьToolStripMenuItem";
+            this.открытьToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.открытьToolStripMenuItem.Text = "Открыть";
+            this.открытьToolStripMenuItem.Click += new System.EventHandler(this.открытьToolStripMenuItem_Click);
+            // 
+            // создатьНовыйToolStripMenuItem
+            // 
+            this.создатьНовыйToolStripMenuItem.Name = "создатьНовыйToolStripMenuItem";
+            this.создатьНовыйToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.создатьНовыйToolStripMenuItem.Text = "Создать новый";
+            this.создатьНовыйToolStripMenuItem.Click += new System.EventHandler(this.создатьНовыйToolStripMenuItem_Click);
+            // 
+            // сохранитьToolStripMenuItem
+            // 
+            this.сохранитьToolStripMenuItem.Name = "сохранитьToolStripMenuItem";
+            this.сохранитьToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.сохранитьToolStripMenuItem.Text = "Сохранить";
+            this.сохранитьToolStripMenuItem.Click += new System.EventHandler(this.сохранитьToolStripMenuItem_Click);
+            // 
+            // печатьToolStripMenuItem
+            // 
+            this.печатьToolStripMenuItem.Name = "печатьToolStripMenuItem";
+            this.печатьToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.печатьToolStripMenuItem.Text = "Печать";
+            this.печатьToolStripMenuItem.Click += new System.EventHandler(this.печатьToolStripMenuItem_Click);
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.выходToolStripMenuItem.Text = "Выход";
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.FontSizeUpDown);
+            this.panel1.Controls.Add(this.underlineBtn);
+            this.panel1.Controls.Add(this.italicBtn);
+            this.panel1.Controls.Add(this.boldBtn);
+            this.panel1.Controls.Add(this.colorBtn);
+            this.panel1.Location = new System.Drawing.Point(16, 32);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(772, 40);
+            this.panel1.TabIndex = 2;
+            // 
+            // colorBtn
+            // 
+            this.colorBtn.Location = new System.Drawing.Point(475, 8);
+            this.colorBtn.Name = "colorBtn";
+            this.colorBtn.Size = new System.Drawing.Size(75, 23);
+            this.colorBtn.TabIndex = 0;
+            this.colorBtn.Text = "Цвет";
+            this.colorBtn.UseVisualStyleBackColor = true;
+            this.colorBtn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // boldBtn
+            // 
+            this.boldBtn.Location = new System.Drawing.Point(348, 8);
+            this.boldBtn.Name = "boldBtn";
+            this.boldBtn.Size = new System.Drawing.Size(32, 23);
+            this.boldBtn.TabIndex = 1;
+            this.boldBtn.Text = "B";
+            this.boldBtn.UseVisualStyleBackColor = true;
+            this.boldBtn.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // italicBtn
+            // 
+            this.italicBtn.Location = new System.Drawing.Point(386, 8);
+            this.italicBtn.Name = "italicBtn";
+            this.italicBtn.Size = new System.Drawing.Size(32, 23);
+            this.italicBtn.TabIndex = 2;
+            this.italicBtn.Text = "I";
+            this.italicBtn.UseVisualStyleBackColor = true;
+            this.italicBtn.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // underlineBtn
+            // 
+            this.underlineBtn.Location = new System.Drawing.Point(424, 8);
+            this.underlineBtn.Name = "underlineBtn";
+            this.underlineBtn.Size = new System.Drawing.Size(32, 23);
+            this.underlineBtn.TabIndex = 3;
+            this.underlineBtn.Text = "U";
+            this.underlineBtn.UseVisualStyleBackColor = true;
+            this.underlineBtn.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // FontSizeUpDown
+            // 
+            this.FontSizeUpDown.Location = new System.Drawing.Point(253, 11);
+            this.FontSizeUpDown.Name = "FontSizeUpDown";
+            this.FontSizeUpDown.Size = new System.Drawing.Size(89, 20);
+            this.FontSizeUpDown.TabIndex = 4;
+            this.FontSizeUpDown.ValueChanged += new System.EventHandler(this.FontSizeUpDown_ValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.mainTextBox);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.FontSizeUpDown)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.RichTextBox mainTextBox;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem открытьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem сохранитьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem печатьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem создатьНовыйToolStripMenuItem;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button underlineBtn;
+        private System.Windows.Forms.Button italicBtn;
+        private System.Windows.Forms.Button boldBtn;
+        private System.Windows.Forms.Button colorBtn;
+        private System.Windows.Forms.NumericUpDown FontSizeUpDown;
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -7,14 +7,203 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Drawing.Printing;
 
 namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
+        String filename;
+        PrintDialog pd;
+        PrintDocument pdoc;
+        string[] lines;
+        int linesPrinted;
         public Form1()
         {
             InitializeComponent();
+            FontSizeUpDown.Maximum = 100;
+            FontSizeUpDown.Minimum = 5;
+            FontSizeUpDown.Value = 12;
+            mainTextBox.SelectionChanged += selectionChanged;
+        }
+        private void selectionChanged(object sender, EventArgs e)
+        {
+            var font = mainTextBox.SelectionFont;
+            FontSizeUpDown.Value = (decimal)font.Size;
+            if (font.Bold)
+                boldBtn.Font = new Font(boldBtn.Font.FontFamily, boldBtn.Font.Size, font.Style);
+            if (font.Italic)
+                italicBtn.Font = new Font(italicBtn.Font.FontFamily, italicBtn.Font.Size, font.Style);
+            if (font.Underline)
+                underlineBtn.Font = new Font(underlineBtn.Font.FontFamily, underlineBtn.Font.Size, font.Style);
+
+        }
+        private void создатьНовыйToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SaveFileDialog fod = new SaveFileDialog();
+                fod.Title = "Создание нового файла";
+                fod.DefaultExt = "*.rtf";
+                if (fod.ShowDialog() != DialogResult.Cancel)
+                {
+                    filename = fod.FileName;
+                    File.Create(filename);
+                    mainTextBox.Clear();
+                    mainTextBox.Enabled = true;
+                }
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message);
+            }
+        }
+
+        private void открытьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var fod = new OpenFileDialog();
+                //fod.DefaultExt = "*.rtf";
+                if (fod.ShowDialog() != DialogResult.Cancel) {
+                    filename = fod.FileName;
+                    mainTextBox.LoadFile(filename);
+                    mainTextBox.Enabled = true;
+                }
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message);
+            }
+        }
+
+        private void сохранитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (filename != null)
+                    mainTextBox.SaveFile(filename);
+                else
+                {
+                    var sfd = new SaveFileDialog();
+                    if (sfd.ShowDialog() == DialogResult.OK)
+                    {
+                        filename = sfd.FileName;
+                        mainTextBox.SaveFile(filename);
+                    }
+                }
+            } 
+             catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Font currentFont = mainTextBox.SelectionFont;
+            FontStyle style;
+            if (!currentFont.Bold)
+                style = FontStyle.Bold;
+            else
+                style = FontStyle.Regular;
+            mainTextBox.SelectionFont = new Font(currentFont.FontFamily, currentFont.Size, style);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Font currentFont = mainTextBox.SelectionFont;
+            FontStyle style;
+            if (!currentFont.Italic)
+                style = FontStyle.Italic;
+            else
+                style = FontStyle.Regular;
+            mainTextBox.SelectionFont = new Font(currentFont.FontFamily, currentFont.Size, style);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Font currentFont = mainTextBox.SelectionFont;
+            FontStyle style;
+            if (!currentFont.Underline)
+                style = FontStyle.Underline;
+            else
+                style = FontStyle.Regular;
+            mainTextBox.SelectionFont = new Font(currentFont.FontFamily, currentFont.Size, style);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ColorDialog cd = new ColorDialog();
+            if (cd.ShowDialog()!=DialogResult.Cancel)
+            {
+                mainTextBox.SelectionColor = cd.Color;
+            }
+            
+        }
+
+        private void FontSizeUpDown_ValueChanged(object sender, EventArgs e)
+        {
+            float size = (float)this.FontSizeUpDown.Value;
+            var currentFont = mainTextBox.SelectionFont;
+            mainTextBox.SelectionFont = new Font(currentFont.FontFamily, size, currentFont.Style);
+        }
+
+        private void печатьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            pd = new PrintDialog();
+            pdoc = new PrintDocument();
+            if (pd.ShowDialog() == DialogResult.OK)
+            {
+                pdoc.Print();
+            }
+
+        }
+
+        // OnBeginPrint 
+        private void OnBeginPrint(object sender,
+                          System.Drawing.Printing.PrintEventArgs e)
+        {
+            char[] param = { '\n' };
+
+            if (pd.PrinterSettings.PrintRange == PrintRange.Selection)
+            {
+                lines = mainTextBox.SelectedText.Split(param);
+            }
+            else
+            {
+                lines = mainTextBox.Text.Split(param);
+            }
+
+            int i = 0;
+            char[] trimParam = { '\r' };
+            foreach (string s in lines)
+            {
+                lines[i++] = s.TrimEnd(trimParam);
+            }
+        }
+        // OnPrintPage
+        private void OnPrintPage(object sender,
+                                   System.Drawing.Printing.PrintPageEventArgs e)
+        {
+            int x = e.MarginBounds.Left;
+            int y = e.MarginBounds.Top;
+            Brush brush = new SolidBrush(mainTextBox.ForeColor);
+
+            while (linesPrinted < lines.Length)
+            {
+                e.Graphics.DrawString(lines[linesPrinted++],
+                     mainTextBox.Font, brush, x, y);
+                y += 15;
+                if (y >= e.MarginBounds.Bottom)
+                {
+                    e.HasMorePages = true;
+                    return;
+                }
+                else
+                    e.HasMorePages = false;
+            }
         }
     }
 }
